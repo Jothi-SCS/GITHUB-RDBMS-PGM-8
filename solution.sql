671af8fa2b467228bab0b420f463c30f1b210d25
Jothi-SCS <54353240+Jothi-SCS@users.noreply.github.com>
2026-08-22 10:46:32 +0530

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -1,13 +1,1 @@
-USE EmployeeDB;
 
-SELECT COUNT(Salary) AS Total_Employees
-FROM Employee;
-
-SELECT MAX(Salary) AS Maximum_Salary
-FROM Employee;
-
-SELECT MIN(Salary) AS Minimum_Salary
-FROM Employee;
-
-SELECT AVG(Salary) AS Average_Salary
-FROM Employee;
